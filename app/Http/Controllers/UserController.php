d00bf27b1f0b7065898967c4428ff0a0725059f2
Sergio Rios <sergioriosp04@gmail.com>
2020-01-29 17:53:01 -0500

    validar el formulario de configuracion

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,12 +12,21 @@ class UserController extends Controller
     }
 
     public function update(Request $request){
+
         $id = \Auth::user()->id;
+
+       $validate = $this->validate($request, [
+           'name' => 'required|string|max:255',
+           'surname' => 'required|string|max:255',
+           'nick' => 'required|string|max:255|unique:users,nick,'.$id,
+           'email' => 'required|string|email|max:255|unique:users,email,'.$id,
+       ]);
+
         $name = $request->input('name');
         $surname = $request->input('surname');
         $nick = $request->input('nick');
         $email = $request->input('email');
 
-        dd($id);
+        //dd($id);
     }
 }
