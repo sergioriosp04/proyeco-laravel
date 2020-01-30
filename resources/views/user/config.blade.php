a1dc2e13b2b9f8b566cfd2f9f286c54d08a2af93
Sergio Rios <sergioriosp04@gmail.com>
2020-01-30 12:23:37 -0500

    mostrat avatar en configuracion de usuario

--- resources/views/user/config.blade.php
+++ resources/views/user/config.blade.php
@@ -74,9 +74,14 @@
                             </div>
 
                             <div class="form-group row">
-                                <label for="image_path" class="col-md-4 col-form-label text-md-right">{{ __('Avatar') }}</label>
 
+                                <label for="image_path" class="col-md-4 col-form-label text-md-right">{{ __('Avatar') }}</label>
                                 <div class="col-md-6">
+                                    @if(Auth::user()->image)
+                                            {{--dos formas de mostrar la imagen--}}
+                                            {{--<img src="{{ url('/user/avatar/'.Auth::user()->image) }}" />--}}
+                                            <img src="{{ route('user.avatar', [ 'filename' => Auth::user()->image]) }}" class="avatar" />
+                                    @endif
                                     <input id="image_path" type="file" class="form-control{{ $errors->has('image_path') ? ' is-invalid' : '' }}" name="image_path" >
 
                                     @if ($errors->has('image_path'))
--- routes/web.php
+++ routes/web.php
@@ -38,3 +38,5 @@ Route::get('/', 'HomeController@index')->name('home');
 Route::get('/configuracion', 'UserController@config')->name('config');
 
 Route::post('/user/update', 'UserController@update')->name('user.update');
+
+Route::get('/user/avatar/{filename}', 'UserController@getImage')->name('user.avatar');
