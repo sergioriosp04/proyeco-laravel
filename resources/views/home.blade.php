7a1090f5130f23e38e5d2b3315b8a0dcf9d40b13
Sergio Rios <sergioandresrios04@gmail.com>
2020-01-30 22:40:59 -0500

    listado de ususarios que tienen imagenes en el home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,19 +4,18 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-                @include('includes.message')
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+            @include('includes.message')
 
-                    You are logged in!
+            @foreach($images as $image)
+                <div class="card">
+                    <div class="card-header">
+                        <img src="{{ route('user.avatar', [ 'filename' => $image->user->image]) }}" class="avatar rounded-circle mr-3" />
+                        <span class="font-weight-bold">{{ $image->user->name. '' .$image->user->username }}</span>
+                    </div>
+                    <div class="card-body">
+                    </div>
                 </div>
-            </div>
+            @endforeach
         </div>
     </div>
 </div>
